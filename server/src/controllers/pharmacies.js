ae1e8d47f11f0880230dbf36495fa7b0ddee47d3
afterglow1251 <157438603+afterglow1251@users.noreply.github.com>
2024-03-10 02:21:39 +0200

    Update pharmacies.js

--- server/src/controllers/pharmacies.js
+++ server/src/controllers/pharmacies.js
@@ -25,31 +25,25 @@ export const postPharmacy = async (request, reply) => {
 
 export const putPharmacy = async (request, reply) => {
   try {
-    const { name, price } = request.body
 
-    if (!name || !price) {
-      return reply.code(400).send('Missing required fields')
-    }
+    const pharmacy = await Pharmacy.findOne({ _id: request.params.id })
 
-    const ph = await Pharmacy.findOne({ _id: request.params.id })
-
-    if (!ph) {
+    if (!pharmacy) {
       return reply.code(404).send('Pharmacy not found')
     }
 
-    ph.medications.push({
-      name: name,
-      price: price,
-      number: request.body.number,
-      img: request.body.img
+    pharmacy.medications.push({
+      name: request.body.medicationName,
+      price: request.body.medicationPrice,
+      number: request.body.medicationNumber,
+      img: request.body.medicationImg
     })
 
-    await ph.save()
+    await pharmacy.save()
 
-    reply.send('OK!')
-  } catch (error) {
-    console.error('Error in putPharmacy:', error)
-    reply.code(500).send('Internal Server Error')
+    reply.send('Medication added successfully!')
+  } catch (err) {
+    reply.send('Error in putPharmacy:', err)
   }
 }
 
@@ -58,7 +52,7 @@ export const getPharmacies = async (request, reply) => {
     const pharmacies = await Pharmacy.find()
     reply.send(pharmacies)
   } catch (err) {
-    reply.send('Error during getting pharmacies')
+    reply.send('Error during getting pharmacies', err)
   }
 }
 
